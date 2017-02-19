cc6d50a3d5324d609cdd9cb14e1adcedc7d8ff7e
Filip Jankoski <filip@agmedia.hr>
2017-02-19 21:02:32 +0100

    Minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="orbit" role="region" aria-label="Landing page slider" data-orbit>
+    <div class="callout large">
+        <div class="row column text-center">
+            <h1>Changing the World Through Design</h1>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris.</p>
+            <a href="#" class="button large">Learn More</a>
+            <a href="#" class="button large hollow">Learn Less</a>
+        </div>
+    </div>
+    
+    {{--<div class="orbit" role="region" aria-label="Landing page slider" data-orbit>
         <ul class="orbit-container">
             <button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
             <button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>
             <li class="orbit-slide is-active">
                 <img src="{{ URL::to('/') }}/images/slider/phone1.jpeg">
+                <div class="caption"><br>Caption goes here</div>
             </li>
             <li class="orbit-slide">
                 <img src="{{ URL::to('/') }}/images/slider/hand1.jpeg">
@@ -15,7 +25,7 @@
                 <img src="{{ URL::to('/') }}/images/slider/sail.jpeg">
             </li>
         </ul>
-    </div>
+    </div>--}}
 
 
     <div class="row content">
